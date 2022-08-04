3ed0a10bc8918cfdf0ad92413d794704dd7768c8
Biju Borah <biju20_ug@ee.nits.ac.in>
2022-08-04 23:39:13 +0530

    deployment goes brrrr

--- app.js
+++ app.js
@@ -1,6 +1,22 @@
 const bodyParser = require("body-parser");
+const { query } = require("express");
+const { writeRecords } = require("./crud")
 var express = require("express");
 var app = express();
+const AWS = require("aws-sdk");
+AWS.config.update({ region: "us-east-1" });
+
+var https = require('https');
+var agent = new https.Agent({
+    maxSockets: 5000
+});
+writeClient = new AWS.TimestreamWrite({
+    maxRetries: 10,
+    httpOptions: {
+        timeout: 20000,
+        agent: agent
+    }
+});
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -9,35 +25,13 @@ app.post("/", (req, res, next) => {
     console.log(req.body)
     res.json(req.body)
 })
-app.get("/", (req, res, next) => {
-    if (req.query.timeFrame == null) {
-        res.json(
-            {
-                data: {
-                    a: 1,
-                    b: 2,
-                    c: 3,
-                    d: 4,
-                    e: 5,
-                }
-            }
-        );
-    }
-    else {
-        res.json(
-            {
-                data: {
-                    a: 1,
-                    b: 2,
-                    c: 3,
-                    d: 4,
-                    e: 5,
-                }
-            }
-        );
-    }
+app.get("/", async (req, res, next) => {
+    const id = req.params.id
+    const timeInterval = req.params.timeInterval
+    await writeRecords(id, timeInterval)
+    res.status(200)
 });
 
 app.listen(process.env.PORT, () => {
-    console.log("Server running ");
+    console.log("Server running......");
 });
---
+++ constant.js
@@ -0,0 +1,6 @@
+// Constants
+const DATABASE_NAME = 'IoT';
+const TABLE_NAME = 'IOT_';
+
+
+module.exports = { DATABASE_NAME, TABLE_NAME };
