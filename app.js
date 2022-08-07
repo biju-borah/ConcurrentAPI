af578733e05daae51f2afc68ef1dfd3d2c905448
Biju Borah <biju20_ug@ee.nits.ac.in>
2022-08-07 11:44:54 +0530

    changed POST/write

--- app.js
+++ app.js
@@ -33,119 +33,24 @@ app.get('/', (req, res, next) => {
 //     res.json(req.body);
 // })
 
-app.get("/write", (req, res, next) => {
-    if (req.query.api_key !== "tPmAT5Ab3j7F9") {
-        res.status(401).json({ err: "Invalid Api Key" });
-        return next();
-    }
-
-
-    const id = String(req.query.id)
-    const timeInterval = String(req.query.timeInterval)
-    const currentTime = Date.now().toString(); // Unix time in milliseconds
-
-    const params = {
-        "DatabaseName": DATABASE_NAME,
-        "Records": [
-            {
-                Dimensions: [
-                    {
-                        Name: 'SensorID',
-                        Value: id,
-                        DimensionValueType: "VARCHAR"
-                    },
-                ],
-
-                'MeasureName': 'data',
-                "MeasureValues": [
-                    {
-                        "Name": "x",
-                        "Type": "DOUBLE",
-                        "Value": "1.2"
-                    },
-                    {
-                        "Name": "y",
-                        "Type": "DOUBLE",
-                        "Value": "1.2"
-                    },
-                    {
-                        "Name": "z",
-                        "Type": "DOUBLE",
-                        "Value": "1.2"
-                    },
-                    {
-                        "Name": "a",
-                        "Type": "DOUBLE",
-                        "Value": "1.2"
-                    },
-                    {
-                        "Name": "b",
-                        "Type": "DOUBLE",
-                        "Value": "1.2"
-                    },
-                    {
-                        "Name": "c",
-                        "Type": "BOOLEAN",
-                        "Value": "True"
-                    },
-                    {
-                        "Name": "m",
-                        "Type": "VARCHAR",
-                        "Value": "life"
-                    },
-                    {
-                        "Name": "n",
-                        "Type": "VARCHAR",
-                        "Value": "is"
-                    },
-                    {
-                        "Name": "p",
-                        "Type": "VARCHAR",
-                        "Value": "beautiful"
-                    }
-                ],
-                'MeasureValueType': 'MULTI',
-
-                "Time": currentTime,
-                "TimeUnit": "MILLISECONDS",
-                "Version": 1
-            }
-        ],
-        "TableName": TABLE_NAME
-    };
-
-    const request = writeClient.writeRecords(params);
-
-    request.promise().then(
-        (data) => {
-            console.log("Write records successful");
-            res.status(200).json(data)
-        },
-        (err) => {
-            console.log("Error writing records:", err);
-            if (err.code === 'RejectedRecordsException') {
-                const responsePayload = JSON.parse(request.response.httpResponse.body.toString());
-                console.log("RejectedRecords: ", responsePayload.RejectedRecords);
-                console.log("Other records were written successfully. ");
-            }
-            res.json(err)
-        }
-    );
-});
-
 app.post("/write", (req, res, next) => {
-    if (req.body === {}) {
+    if (!Object.keys(req.body).length || !Object.keys(req.body.data).length) {
         res.status(200).json({ err: "Bad request" });
         return next();
     }
 
-    if (req.body.api_key !== "tPmAT5Ab3j7F9") {
+    if (!req.body.hasOwnProperty("sensor") || req.body.sensor === "" || req.body.sensor === undefined) {
+        res.status(200).json({ err: "Bad request" });
+        return next();
+    }
+
+    if (req.body.api_key === undefined || req.body.api_key !== "tPmAT5Ab3j7F9") {
         res.status(401).json({ err: "Invalid Api Key" });
         return next();
     }
 
-    const id = String(req.body.sensorID)
-    const x = String(req.body.x)
+    const id = String(req.body.sensor)
+    const data = req.body.data;
     const timeInterval = String(req.body.timeInterval)
     const currentTime = Date.now().toString(); // Unix time in milliseconds
 
@@ -155,7 +60,7 @@ app.post("/write", (req, res, next) => {
             {
                 Dimensions: [
                     {
-                        Name: 'SensorID',
+                        Name: 'sensor',
                         Value: id,
                         DimensionValueType: "VARCHAR"
                     },
@@ -166,47 +71,47 @@ app.post("/write", (req, res, next) => {
                     {
                         "Name": "x",
                         "Type": "DOUBLE",
-                        "Value": x
+                        "Value": String(data.x)
                     },
                     {
                         "Name": "y",
                         "Type": "DOUBLE",
-                        "Value": "1.2"
+                        "Value": String(data.y)
                     },
                     {
                         "Name": "z",
                         "Type": "DOUBLE",
-                        "Value": "1.2"
+                        "Value": String(data.z)
                     },
                     {
                         "Name": "a",
                         "Type": "DOUBLE",
-                        "Value": "1.2"
+                        "Value": String(data.a)
                     },
                     {
                         "Name": "b",
                         "Type": "DOUBLE",
-                        "Value": "1.2"
+                        "Value": String(data.b)
                     },
                     {
                         "Name": "c",
                         "Type": "BOOLEAN",
-                        "Value": "True"
+                        "Value": String(data.c)
                     },
                     {
                         "Name": "m",
                         "Type": "VARCHAR",
-                        "Value": "life"
+                        "Value": String(data.m)
                     },
                     {
                         "Name": "n",
                         "Type": "VARCHAR",
-                        "Value": "is"
+                        "Value": String(data.n)
                     },
                     {
                         "Name": "p",
                         "Type": "VARCHAR",
-                        "Value": "beautiful"
+                        "Value": String(data.p)
                     }
                 ],
                 'MeasureValueType': 'MULTI',
