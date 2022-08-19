afb71b640c31771ff53af99b1d783d57d1726b41
Biju Borah <biju20_ug@ee.nits.ac.in>
2022-08-20 01:28:58 +0530

    Update app.js

--- app.js
+++ app.js
@@ -171,6 +171,8 @@ app.get('/fetch', (req, res, next) => {
                     var dateUTC = new Date(datas.data[t].time);
                     var dateUTC = dateUTC.getTime()
                     var dateIST = new Date(dateUTC);
+                    dateIST.setHours(dateIST.getHours() - 5);
+                    dateIST.setMinutes(dateIST.getMinutes() - 30);
                     dateIST.setSeconds(dateIST.getSeconds() - 30 * multi)
                     data["time"] = dateIST.toLocaleString(undefined, { timeZone: 'Asia/Kolkata' });
                     multi += 1
