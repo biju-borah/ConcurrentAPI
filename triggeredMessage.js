77b9fbb5bc7f60fed97104f63affb70c06029398
Moloy Writ Saikia <46087868+writ-tech@users.noreply.github.com>
2022-09-11 00:03:42 +0530

    Update triggeredMessage.js

--- triggeredMessage.js
+++ triggeredMessage.js
@@ -1,3 +1,3 @@
 module.exports = {
-    msg: "pH value is below 5.5. Visit : http://ministerpaneljjm.s3-website-us-east-1.amazonaws.com "
+    msg: "pH value is below 5.5 for this address- [Sonitpur, PHE Division 1, Tezpur, Majgaon PWSS] . Visit : http://ministerpaneljjm.s3-website-us-east-1.amazonaws.com "
 }
